492d8245f29b231f33505824dfbfa9d7a7d8df83
Frooks <frooks@tutanota.com>
2022-08-17 05:42:01 +0200

    SECOND COMMIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['./src/**/*.{html,js,ts}'],
   plugins: [require('daisyui')],
+  theme: {
+    screens: {
+      'sm': '812px',
+      // => @media (min-width: 576px) { ... }
 
+      'md': '812px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+
+      'xl': '1680px',
+      // => @media (min-width: 1440px) { ... }
+    }
+  },
   daisyui: {
     themes: ["valentine"],
   },
