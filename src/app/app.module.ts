ccdc8d34770e7319414dd01d4eac32d605c750f8
Frooks <frooks@tutanota.com>
2022-08-28 02:04:22 +0200

    refactor + routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HomeComponent} from './components/home/home.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
-  ],
-  declarations: [AppComponent, ProductListComponent, FooterComponent, HomeComponent, NavbarComponent],
-  bootstrap: [AppComponent, FooterComponent, HomeComponent, NavbarComponent],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot([{path: '', component: AppComponent}]),
+        AppRoutingModule,
+    ],
+    declarations: [AppComponent, FooterComponent, HomeComponent, NavbarComponent],
+    bootstrap: [AppComponent, FooterComponent, HomeComponent, NavbarComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
